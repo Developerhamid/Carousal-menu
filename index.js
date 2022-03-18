16f097b43078a307a00fbeaf04c1e1832df7f6fa
Developerhamid <iamdeveloperhamid@gmail.com>
2022-03-18 22:33:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,186 @@
+        const AkastukiData = [
+            {
+                id: 1,
+                name: "Itachi Uchiha",
+                info: "Itachi is one of the main villains in Naruto, as the murderer of the entire Uchiha Clan. He's the older brother of Sasuke Uchiha and also the reason for Sasuke's need for more power, which resulted in his defection from The Leaf in search of Orochimaru. Itachi is a member of the akatsuki organization and is partnered with Kisame Hoshigaki, a former member of the seven ninja swordsmen of the hidden mist. Before joining the Akatsuki and defecting the leaf village, Itachi was a member of the hidden leaf's Anbu",
+                skill: "Doujutsu - This kind of Jutsu involves using ones eyes, and will recognize othersGenjutsu, Ninjutsu , andTaijutsu techniques to form a counter attack. Two examples of Doujutsu attacks would be Sharingan and White Eyes. Typically the only people who are able to use Doujutsu attacks are people born into a family that already knows it. Because these attacks don't require much chakra they can be used from long amounts of time, without tiring the user.",
+                img: 
+                './img/uchiha_itachi.png'
+            },
+            {
+                id: 2,
+                name: "Deidara",
+                info: "A former ninja from The Hidden Rock Village and terrorist bomber. He was recruited and forced to join the elite criminal organization Akatsuki as one of it's members. An expert in the use of chakra charged explosive clay, and can mold it into whatever shape desired. He takes great pride in his works of art and becomes very sensitive whenever someone insults it. He is partnered with Sasori in the Akatsuki Organization, a fellow artist.",
+                skill: "Deidara has a very unique ability, he uses his hands where his palms posses mouths that when feed a certain clay explosive can create a flying bird or many small spiders that can be controlled or control them self after which will explode on command or when they deem suitable.",
+                img: 
+                './img/deidara.jpg'
+            },
+            {
+                id: 3,
+                name: "Hidan",
+                info: "Hidan is a member of the criminal organization known as akatsuki in the anime and manga series Naruto. He is special because he is immortal. He is a very religious man and often uses obscene language. He is more devoted to his religion than the goals of the Akatsuki.",
+                skill: "Hidan's most powerful technique allows him to take control of the enemies body. This techniques works when HIdan cuts his enemy with his 3 bladed sword, after drawing blood hidan will consume it then draw a symbol in the ground which will start the technique. With this technique if Hidan stabs him self the enemy will be stabbed and suffer the same injury, because of his immortal technique nothing happens to Hidan",
+                img: 
+                './img/hidan.png'
+            },
+            {
+                id: 4,
+                name: "Jugo",
+                info: "Juugo, naturally nice and gentle, but has homicidal urges due to the Curse Mark. He sought Orochimaru to cure him and was locked away, Orochimaru instead found a way to give others the Curse Mark. He is now with Sasuke.",
+                skill: "nothing special.",
+                img: 
+                './img/jugo_unsg.png'
+            },
+            {
+                id: 5,
+                name: "Kabuto",
+                info: "A Jounin from the Hidden sound Village. He serves as Orochimaru's spy and is a skilled medical Ninja.",
+                skill: "Follows Orochimaru orders",
+                img: 
+                './img/kabuto.jpg'
+            },
+            {
+                id: 6,
+                name: "Kakuzu",
+                info: "Kakuzu is a member of the criminal organization known as Akatsuki. He is the accountant for the Akatsuki which makes him obsessed with money, he also possesses five hearts. He once fought the first hokage Hashirama and lost. He is a former shinobi from the village hidden in the waterfalls.",
+                skill: "Kakuzu can use his string to shoot out limbs off his body to attack enemies,  Kakuzus ultimate attack is to use the string with his body and form large objects that cab allow him to fly for example.",
+                img: 
+                './img/kakuzu.png'
+            },
+            {
+                id: 7,
+                name: "Karin",
+                info: "Karin was a guard in charge of one of Orochimaru's prison labs. Sasuke invited her to join his team because of her tracking abilities, but she'd rather ditch the others and have Sasuke all to herself.",
+                skill: "Medical ninjutsu",
+                img: 
+                './img/karin_unsg.png'
+            },
+            {
+                id: 8,
+                name: "Kisame Hoshigaki",
+                info: "A former ninja from The Hidden Mist Village and was a former member of the Seven Ninja Swordsmen of The Mist. After defecting from his village, he joined the elite criminal organization Akatsuki. He is partnered up with Itachi Uchiha and his main weapon is Samehada (Sharkskin).",
+                skill: "Kisame is Itachi's partner, replacing Orochimaru. His specialty is the use of water jutsu and also uses his trademark sword, the Samehada, which only he can yield.",
+                img: 
+                './img/kisame_hoshigaki.jpg'
+            },
+            {
+                id: 9,
+                name: "Konan",
+                info: "Pain's partner in Akatsuki, the only member besides Tobi/Madara that addresses him as Pain. The only female member of Akatsuki. She is also known as angel by the people of Amegakure.",
+                skill: "Konan posses techniques that allows her to control paper and create paper thousands of sheets of paper with her ninjutsu she can also transform or add to her body, like wings for example. Konan and her paper wing Jutsu she can also wrap her opponents in sheets of paper trapping them and restricting them from using any ninjutsu techniques. This technique can be weaken if for example a liquid is applied to the paper also Shino's bugs seemed to be affective against the paper.",
+                img: 
+                './img/konan.jpg'
+            },
+            {
+                id: 10,
+                name: "Obito Uchiha",
+                info: "A character from the anime series Naruto. He claimed to be Madara Uchiha the man who fought the First Hokage, Hashirama. He is the main antagonist of the Naruto Series. He also goes by the name of Tobi. He is a former student of the fourth hokage, Minato. He is responsible for the Nine-Tails Attack on Konoha as well as the Uchiha clan massacre, ruining both Naruto and Sasuke's lives. His best friend is Kakashi Hatake and his love interest is Rin Nohara..",
+                skill: "Tobi has displayed a few jutsu thus far, such as an unnamed teleporting jutsu which he can use to teleport himself or others, a related unnamed phasing jutsu, an unnamed katon, Mole Hiding. He possess many unique jutsu that only he knows, Including the ability to transport himself, parts of his body and others to different dimensions.",
+                img: 
+                './img/madara_uchiha.jpg'
+            },
+            {
+                id: 11,
+                name: "Orochimaru",
+                info: "Orochimaru is one of the legendary Sannin ninja and one of the main villains in the Naruto series. He is voiced by very talented voice actor Steve Blum. He is a student of the third hokage, Sarutobi. He tried to take the bodies of Sasuke and Itachi Uchiha, but failed. Orochimaru was once a member of the Akatsuki.",
+                skill: "In the video games his moves usually revolve around snake attacks, fire breathing and more recently the sword that he keeps in his throat. He is known for being one of the more powerful characters like fellow Sanin Jiraiya and Tsunade.",
+                img: 
+                './img/orochimaru.jpg'
+            },
+            {
+                id: 12,
+                name: "Pain",
+                info: "Pain is the recognised leader of Akatsuki and leader of Amegakure. He wields the legendary Rinnegan and refers to himself as a god. His real name is revealed to be Nagato and was once orphaned by war along with his friends Yahiko and Konan. The Three were trained by Jiraiya in Ninjutsu.",
+                skill: "Pains power stems from this powerful technique, the only other person known to posses this eye technique is the first shinobi ever, sage of the six paths. Rinnegan allows Pain to use all six forms of nature manipulation",
+                img: 
+                './img/nagato_six_paths_of__pain.jpg'
+            },
+            {
+                id: 13,
+                name: "Sasori",
+                info: "A rougue ninja from The Hidden Sand Village. After defection, he joined the elite criminal organization Akatsuki. He is considerd the father of all current ninja puppet techniques, as he was the one who created many of the currently used techniques and puppets in The Hidden Sand Village.",
+                skill: "Sasori has a number of techniques and puppets, the first seen is his HIruko puppet Armour. Other techniques are the Kazekage puppet which he fuses with a sand technique jutsu, Sasoris most powerful technique is the Red secret technique: performance of a thousand puppets. This is were Sasori can control up to one thousand puppets, he controls the puppets not with his hands but with a different technique. Sasori will open the compartment on his right side where strings of chakra based control string will be realed and start to control the puppets.",
+                img: 
+                './img/sasori.jpg'
+            },
+            {
+                id: 14,
+                name: "Sasuke Uchiha",
+                info: "Naruto's rival, but also closest friend. He's an integral character in the Naruto world. He is Itachi Uchiha's younger brother. He is also a former member of Team 7. After defeating Itachi, he loses Orochimaru's curse mark and is told the truth of the Uchiha's destruction and of Itachi's secret life by Tobi.",
+                skill: "Chidori",
+                img: 
+                './img/sasuke2_nar_691.png'
+            },
+            {
+                id: 15,
+                name: "Suigetsu Hozuki",
+                info: "Suigetsu is from the Hidden Mist village and was being trained by Zabuza to be one of the Seven Swordsmen. He now wields Zabuza's sword.",
+                skill: "nothing special",
+                img: 
+                './img/suigetsu.jpg'
+            },
+            {
+                id: 16,
+                name: "Yahiko",
+                info: "He is a close friend Konan and Nagato(Pain)also he was convinced by Madara Uchiha to form akatsuki.After his death his body was used as Deva path of Nagato's six paths of pain",
+                skill: "Almighty push",
+                img: 
+                './img/pain.jpg'
+            },
+            {
+                id: 17,
+                name: "Zetsu",
+                info: "Zetsu, the only member of Akatsuki that works alone, he is a scouter who tracks and looks out for enemy.",
+                skill: "Zetsu's abilities are unknown of as yet, but two abilities have been shown one that he is able to merge with the ground and travel to another location instantly, the other being he can record information e.g the battle between sasuke and itachi.",
+                img: 
+                './img/zetsubig.jpg'
+            }
+        ];
+
+    const nextBtn = document.getElementById('nextBtn');
+    const preBtn = document.getElementById('preBtn');
+    const randomBtn = document.getElementById('randomBtn');
+
+    const personImg = document.querySelector('#person-img');
+    const characterName = document.querySelector('#name');
+    const info = document.querySelector('#info');
+    const impSkill = document.querySelector('#imp-skill');
+
+        let characterCount = 0;
+
+    window.addEventListener('DOMContentLoaded',function() {
+        characterChange(characterCount);
+    });
+
+    function characterChange(characterNum){
+      const items = AkastukiData[characterNum];
+      personImg.src = items.img;
+      characterName.textContent = items.name;
+      info.textContent = items.info;
+      impSkill.textContent = items.skill;
+    };
+
+
+    nextBtn.addEventListener('click',() =>{
+        characterCount++;
+        if(characterCount > AkastukiData.length - 1){
+            characterCount = 0;
+        }
+        characterChange(characterCount);
+    });
+
+    preBtn.addEventListener('click',() =>{
+        characterCount--;
+        if(characterCount < 0){
+            characterCount = AkastukiData.length -1;
+        }
+        characterChange(characterCount);
+    });
+
+    randomBtn.addEventListener('click', () =>{
+        let randomNum = Math.floor(Math.random() * AkastukiData.length);
+
+        characterCount = randomNum;
+
+        characterChange(characterCount);
+    });
